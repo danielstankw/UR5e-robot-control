be37a3c78bf6bc87d4efad948ee13124fd48b4b7
danieln7 <danielstankowski@gmail.com>
2023-01-15 15:32:58 +0200

    Partially implemented spiral and circule - donest work

--- run_robot_with_spiral.py
+++ run_robot_with_spiral.py
@@ -15,6 +15,37 @@ from Controller import Controller
 # # ------ this for stopping the while loop with Ctrl+C (KeyboardInterrupt) this has to be at the top of the file!!--------
 # os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = 'T'
 
+def next_spiral(theta_current):
+    # according to the article to assure successful insertion: p<=2d
+    # where p is distance between consequent rings and d is clearance in centralized peg
+    # v = 0.0009230 / 2   # total velocity (linear and angular)
+    v = 0.0025 / 1.5
+    p = 2 * 0.0003  # distance between the consecutive rings
+    dt = 0.002
+
+    theta_dot_current = (2 * np.pi * v) / (p * np.sqrt(1 + theta_current ** 2))
+    # todo: change +/- depending on the desired direction
+    theta_next = theta_current + theta_dot_current * dt
+
+    radius_next = (p / (2 * np.pi)) * theta_next
+
+    x_next = radius_next * np.cos(theta_next)
+    y_next = radius_next * np.sin(theta_next)
+    return theta_next, radius_next, x_next, y_next
+def next_circle(theta_current, dt):
+    # overlap=error-2*radius_of_circle
+    time_per_circle = 5
+    # 0.0025, 0.0026, 0.0027 with perturbation
+    # 0.0028 always enters
+    radius_of_circle = 0.03
+    # increase in angle per dt
+    d_theta = (2 * np.pi * dt) / time_per_circle
+    theta_next = theta_current + d_theta
+
+    x_next = radius_of_circle * np.cos(theta_next) - radius_of_circle
+    y_next = radius_of_circle * np.sin(theta_next)
+
+    return theta_next, radius_of_circle, x_next, y_next
 def circular_wrench_limiter(wrench_cmd):
     # Limit the wrench in TOOL frame in a circular way. meaning that Fxy and Mxy consider as a vector with limited radius
     wrench_safety_limits = dict(Fxy=20, Fz=15, Mxy=4, Mz=3)
@@ -38,41 +69,31 @@ def circular_wrench_limiter(wrench_cmd):
 
     return limited_wrench
 
-def next_spiral(self, theta_current):
-    # according to the article to assure successful insertion: p<=2d
-    # where p is distance between consequent rings and d is clearance in centralized peg
-    # v = 0.0009230 / 2   # total velocity (linear and angular)
-    v = 0.0025 / 1.5
-    p = 2 * 0.0003  # distance between the consecutive rings
-    dt = 0.002
+def external_calibrate(external_sensor_tool, current_pose):
+    external_sensor_force = at.Tool2Base_vec(current_pose[3:], external_sensor_tool[:3])
+    external_sensor_moment = at.Tool2Base_vec(current_pose[3:], external_sensor_tool[3:])
+    external_sensor_base = np.append(external_sensor_force, external_sensor_moment)
+    return external_sensor_base
 
-    theta_dot_current = (2 * np.pi * v) / (p * np.sqrt(1 + theta_current ** 2))
-    # todo: change +/- depending on the desired direction
-    theta_next = theta_current + theta_dot_current * dt
-
-    radius_next = (p / (2 * np.pi)) * theta_next
-
-    x_next = radius_next * np.cos(theta_next)
-    y_next = radius_next * np.sin(theta_next)
-
-    return theta_next, radius_next, x_next,
-
-def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_dim, use_impedance, plot_graphs):
+def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_dim, use_impedance, plot_graphs, circle):
     # Check if the UR controller is powered on and ready to run.
     if use_impedance:
-        Controller(control_dim=control_dim)
+        control = Controller(control_dim=control_dim)
     robot.reset_error()
-    robot.set_payload_mass(m=1.12)
-    robot.set_payload_cog(CoG=(0.005, 0.00, 0.084))
+    # robot.set_payload_mass(m=1.12)
+    # robot.set_payload_cog(CoG=(0.005, 0.00, 0.084))
+    FT_sensor = Onrobot.FT_sensor()
 
     #  move above the hole
     robot.movel(start_pose)
     time.sleep(0.5)  # Wait for the robot/measurements to be stable before starting the insertion
+    robot.zero_ftsensor()
+    robot.force_mode_set_damping(0)
 
     pose_init = np.array(robot.get_actual_tcp_pose(wait=True))
     vel_init = np.array(robot.get_actual_tcp_speed(wait=False))
 
-    time_insertion = 0
+    time_insertion = 40
     time_trajectory = 5
     time_for_simulation = time_trajectory + time_insertion
 
@@ -83,6 +104,9 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
     [position_ref, orientation_ref, lin_vel_ref, ang_vel_ref] = planner.trajectory_planning(0)
     desired_pos = np.concatenate((position_ref, orientation_ref, lin_vel_ref, ang_vel_ref), axis=0)
 
+    # initialize impedance parameters
+    pose_mod = np.zeros(6)
+    vel_mod = np.zeros(6)
     # ----------- Control Settings -------------------
     # Control params for the free space:
     kp = np.array([2250.0, 2250.0, 2250.0, 50.0, 50.0, 50.0])
@@ -90,6 +114,13 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
 
     # ------------ Forces initialization ------------
     internal_sensor_bias = np.copy(robot.get_tcp_force(wait=True))
+    external_sensor_bias_tool = np.copy(FT_sensor.force_moment_feedback())
+    # TODO: Is order correct????
+    external_sensor_bias_base = external_calibrate(external_sensor_bias_tool, pose_init)
+
+    print(f'internal sensor reading (base) = {internal_sensor_bias}')
+    print(f'external sensor reading (tool) = {external_sensor_bias_tool}')
+    print(f'external sensor reading (base) = {external_sensor_bias_base}')
 
     # plotting
     if True:
@@ -104,17 +135,15 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
         vel_min_jerk_x, vel_min_jerk_y, vel_min_jerk_z = [], [], []
         ori_min_jerk_x, ori_min_jerk_y, ori_min_jerk_z = [], [], []
         ori_vel_min_jerk_x, ori_vel_min_jerk_y, ori_vel_min_jerk_z = [], [], []
-        # impedance
-        impedance_pos_vec_x, impedance_pos_vec_y, impedance_pos_vec_z = [], [], []
-        impedance_ori_vec_x, impedance_ori_vec_y, impedance_ori_vec_z = [], [], []
-        impedance_vel_vec_x, impedance_vel_vec_y, impedance_vel_vec_z = [], [], []
-        impedance_ori_vel_vec_x, impedance_ori_vel_vec_y, impedance_ori_vel_vec_z = [], [], []
         # wrench - based on PD
         applied_wrench_fx, applied_wrench_fy, applied_wrench_fz = [], [], []
         applied_wrench_mx, applied_wrench_my, applied_wrench_mz = [], [], []
         # sensor readings
         sensor_fx, sensor_fy, sensor_fz = [], [], []
         sensor_mx, sensor_my, sensor_mz = [], [], []
+        # spiral
+        spiral_x, spiral_y = [], []
+        robot_spiral_x, robot_spiral_y = [], []
 
     try:
         # ------------ Simulation(Loop) setup ----------------
@@ -124,7 +153,7 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
 
         t_init = time.time()
         t_curr = 0
-        made_contact = False
+        contact_flag = False
         contact_time = 0
         f0 = np.zeros(6)
 
@@ -134,6 +163,9 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
         deviation_from_goal_z = 0.002  # 0.004  # Deviation from final goal in [m] which break the loop
         deviation_from_goal_xy = 0.005  # 0.0015
 
+        theta_current = 0
+        x_spiral_next =0
+        y_spiral_next = 0
         #   *   *   *   *   *   *   *   *   = = = = = = = Operation Loop = = = = = = = =    *   *   *   *   *   *   *
         # print('\n* * * * * * *  =  =  =  =  =  =  Simulation Begin =  =  =  =  =  =  * * * * * * *')
         try:
@@ -141,7 +173,7 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
                 t_prev = t_curr
                 t_curr = time.time() - t_init
                 dt = t_curr - t_prev  # 1/125
-                # print(f'\nt = {t_curr} (dt ={dt}):')
+                print(f'\nt = {t_curr} (dt ={dt}):')
 
                 # ---------- Read Sensors -------------------
                 ee_pose = robot.get_actual_tcp_pose(wait=True)  # [x,y,z,rx,ry,rz] - ri: axis-angles
@@ -150,16 +182,25 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
 
                 # ----------- F/T calibration ----------------
                 f_int = internal_sensor_reading - internal_sensor_bias
-                print(t_curr)
+
+                external_sensor_tool = FT_sensor.force_moment_feedback() - external_sensor_bias_tool
+                f_ext = external_calibrate(external_sensor_tool, ee_pose)
+                # print(f'f_int = {f_int} [N]')
+                # print(f'f_ext = {f_ext} [N]')
 
                 # detect contact with the surface
-                if np.abs(f_int[2]) > contact_fz_threshold and made_contact is False:
+                if np.abs(f_int[2]) > contact_fz_threshold and contact_flag is False:
                     print('%%%%%%%%%% Contact established %%%%%%%%%%%')
                     contact_time = t_curr
-                    made_contact = True
-                    impedance_vec = deepcopy(desired_pos)
-                    kp = np.array([5000.0, 5000.0, 250.0, 450.0, 450.0, 450.0])
-                    kd = 2 * np.sqrt(kp) * np.sqrt(2)
+                    contact_flag = True
+                    print('initializing contact pd params')
+                    # initialize impedance params
+                    pose_mod = deepcopy(desired_pos[:6])
+                    vel_mod = deepcopy(desired_pos[6:])
+                    # contact pd parameters
+                    kp = np.array([2250.0 * 4, 2250.0 * 4, 1250.0, 50.0, 50.0, 50.0])
+                    kd = 2 * np.sqrt(kp) * 0.707
+                    # robot.force_mode_set_damping(0.5)
 
                 # ------------- Minimum Jerk Trajectory updating ----------------------
                 # Check if updating reference values with the minimum-jerk trajectory is necessary
@@ -168,13 +209,37 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
                     desired_pos = np.concatenate((position_ref, orientation_ref, lin_vel_ref, ang_vel_ref), axis=0)
 
                 # when contact is established
-                if made_contact:
-                    if use_impedance:
-                        desired_pos = deepcopy(
-                            Controller.impedance_equation(f_int, f0, desired_pos[:3], desired_pos[3:6],
-                                                          desired_pos[6:9], desired_pos[9:12], dt))
+                if contact_flag:
+                    if circle:
+                        """Circle mode"""
+                        theta_next, radius_next, x_spiral_next, y_spiral_next = next_circle(theta_current, dt)
 
+                        spiral_x.append(x_spiral_next + desired_pos[0])
+                        spiral_y.append(y_spiral_next + desired_pos[1])
+                    else:
+                        """Spiral Search mode"""
+                        theta_next, radius_next, x_spiral_next, y_spiral_next = next_spiral(theta_current)
+                        # add shift to the spiral search which is planned at (0,0)
+                        spiral_x.append(x_spiral_next + desired_pos[0])
+                        spiral_y.append(y_spiral_next + desired_pos[1])
+
+                    theta_current = deepcopy(theta_next)
+
+                    # we collect spiral trajectory at this point to exclude everything before contact was made
+                    robot_spiral_x.append(ee_pose[0])
+                    robot_spiral_y.append(ee_pose[1])
+                    print('Spiral')
+
+                    if use_impedance:
+                        # f_int or f_ext
+                        X_next = control.impedance_equation(pose_ref=desired_pos[:6], vel_ref=desired_pos[6:],
+                                                            pose_mod=pose_mod, vel_mod=vel_mod,
+                                                            f_int=f_ext, f0=f0, dt=dt)
+                        desired_pos = deepcopy(X_next)
+                        pose_mod = X_next[:6]
+                        vel_mod = X_next[6:]
                 # ----------- - - - - = = = Control = = = - - - - -------------
+                desired_pos[:2] += np.array([x_spiral_next, y_spiral_next])
                 ori_real = at.AxisAngle_To_RotationVector(pose_desired[3:], ee_pose[3:])
 
                 # Compute desired force and torque based on errors
@@ -184,14 +249,14 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
                 vel_pos_error = desired_pos[6:9].T - ee_vel[:3]
                 vel_ori_error = desired_pos[9:12] - ee_vel[3:]
 
-                # # For PD only (without impedance):
-                print('Using PD Controller')
                 desired_force = (np.multiply(np.array(position_error), np.array(kp[0:3]))
                                  + np.multiply(vel_pos_error, kd[0:3]))
 
                 desired_torque = (np.multiply(np.array(ori_error), np.array(kp[3:6]))
                                   + np.multiply(vel_ori_error, kd[3:6]))
-                wrench_task = np.concatenate([desired_force, desired_torque])
+
+                compensation = deepcopy(internal_sensor_bias)
+                wrench_task = np.concatenate([desired_force, desired_torque]) - compensation
 
                 # ---------------- Sending the wrench to the robot --------------------
                 # print('wrench_task:', wrench_task)
@@ -230,20 +295,7 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
                     ori_vel_min_jerk_x.append(desired_pos[9])
                     ori_vel_min_jerk_y.append(desired_pos[10])
                     ori_vel_min_jerk_z.append(desired_pos[11])
-                    # impedance
-                    impedance_pos_vec_x.append(desired_pos[0])
-                    impedance_pos_vec_y.append(desired_pos[1])
-                    impedance_pos_vec_z.append(desired_pos[2])
-                    impedance_ori_vec_x.append(desired_pos[3])
-                    impedance_ori_vec_y.append(desired_pos[4])
-                    impedance_ori_vec_z.append(desired_pos[5])
-                    impedance_vel_vec_x.append(desired_pos[6])
-                    impedance_vel_vec_y.append(desired_pos[7])
-                    impedance_vel_vec_z.append(desired_pos[8])
-                    impedance_ori_vel_vec_x.append(desired_pos[9])
-                    impedance_ori_vel_vec_y.append(desired_pos[10])
-                    impedance_ori_vel_vec_z.append(desired_pos[11])
-                    # # wrench - based on PD
+                    # applied wrench
                     applied_wrench_fx.append(wrench_safe[0])
                     applied_wrench_fy.append(wrench_safe[1])
                     applied_wrench_fz.append(wrench_safe[2])
@@ -309,25 +361,36 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
     # ****** = = = = = = = = = = * * * * * Plots Section * * * * * = = = = = = = = = = = = *******
     if plot_graphs:
         t = time_vec
+
+        plt.figure("Spiral")
+        plt.plot(spiral_x, spiral_y, 'g', label='Ref position')
+        plt.plot(robot_spiral_x, robot_spiral_y, 'b', label='Robot position')
+        plt.plot(real_goal_pose[0], real_goal_pose[1], "ro")
+        plt.plot(spiral_x[0], spiral_y[0], "go")
+        plt.plot(robot_spiral_x[0], robot_spiral_y[0], "bo")
+        plt.legend()
+        plt.grid()
+
+
         plt.figure("Position")
         ax1 = plt.subplot(311)
-        ax1.plot(t, impedance_pos_vec_x, 'g--', label='Xm position')
         ax1.plot(t, ee_pos_x_vec, 'b', label='Xr position')
         ax1.plot(t, pos_min_jerk_x, 'r--', label='X_ref position')
+        ax1.axvline(x=contact_time, color='k', linestyle='--', label=f"Time of contact: {np.round(contact_time,2)}")
         ax1.legend()
         ax1.set_title('X Position [m]')
 
         ax2 = plt.subplot(312)
-        ax1.plot(t, impedance_pos_vec_y, 'g--', label='Ym position')
         ax2.plot(t, ee_pos_y_vec, 'b', label='Yr position')
         ax2.plot(t, pos_min_jerk_y, 'r--', label='Y_ref position')
+        ax2.axvline(x=contact_time, color='k', linestyle='--', label=f"Time of contact: {np.round(contact_time,2)}")
         ax2.legend()
         ax2.set_title('Y Position [m]')
 
         ax3 = plt.subplot(313)
-        ax1.plot(t, impedance_pos_vec_z, 'g--', label='Zm position')
         ax3.plot(t, ee_pos_z_vec, 'b', label='Zr position')
         ax3.plot(t, pos_min_jerk_z, 'r--', label='Z_ref position')
+        ax3.axvline(x=contact_time, color='k', linestyle='--', label=f"Time of contact: {np.round(contact_time,2)}")
         ax3.legend()
         ax3.set_title('Z Position [m]')
         ################################################################################################################
@@ -392,18 +455,24 @@ def run_robot_with_spiral(robot, start_pose, pose_desired, pose_error, control_d
         ax1 = plt.subplot(311)
         ax1.plot(t, sensor_fx, 'b', label='Fx_sensor')
         ax1.plot(t, applied_wrench_fx, 'g', label='Fx_wrench')
+        ax1.axvline(x=contact_time, color='k', linestyle='--', label=f"Time of contact: {np.round(contact_time,2)}")
+        ax1.grid()
         ax1.legend()
         ax1.set_title('Fx [N]')
 
         ax2 = plt.subplot(312)
         ax2.plot(t, sensor_fy, 'b', label='Fy_sensor')
         ax2.plot(t, applied_wrench_fy, 'g', label='Fy_wrench')
+        ax2.axvline(x=contact_time, color='k', linestyle='--', label=f"Time of contact: {np.round(contact_time,2)}")
+        ax2.grid()
         ax2.legend()
         ax2.set_title('Fy [N]')
 
         ax3 = plt.subplot(313)
         ax3.plot(t, sensor_fz, 'b', label='Fz_sensor')
         ax3.plot(t, applied_wrench_fz, 'g', label='Fz_wrench')
+        ax3.axvline(x=contact_time, color='k', linestyle='--', label=f"Time of contact: {np.round(contact_time,2)}")
+        ax3.grid()
         ax3.legend()
         ax3.set_title('Fz [N]')
         ################################################################################################################
